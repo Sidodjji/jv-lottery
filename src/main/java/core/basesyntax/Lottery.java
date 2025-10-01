27d74611ecd951e8114cbbfb731a4313d1afd1e5
Andrii <andreymaluk35@gmail.com>
2025-10-01 20:51:50 +0300

    Make lottery logic

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery extends  ColorSupplier{
+    public String getRandomBall() {
+        int randomNubmer = new Random().nextInt(100);
+        return getRandomColor() + " " + randomNubmer;
+    }
+}
